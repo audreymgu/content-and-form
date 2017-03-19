75762936a07a30603b8bc960348484a2c3e1284f
George Gu <george.zp.gu@gmail.com>
2017-03-19 02:39:42 -0400

    Created initial subpage template.

--- js/sitewide.js
+++ js/sitewide.js
@@ -1,1 +1,7 @@
+$(window).scroll(function(){
+	$(".background").css("opacity", 1 - $(window).scrollTop() / 225);
+});
 
+$(window).scroll(function(){
+	$(".headerbackground").css("opacity", $(window).scrollTop() / 600);
+});
