7ca4f8e28f8f9aa488ea4398afd85677f9bfd82f
George Gu <george.zp.gu@gmail.com>
2017-05-16 19:46:50 -0400

    Initial state on submission.

--- js/sitewide.js
+++ js/sitewide.js
@@ -1,7 +1,15 @@
 $(window).scroll(function(){
-	$(".background").css("opacity", 1 - $(window).scrollTop() / 225);
+	$(".backgroundoverview").css("opacity", 1 - $(window).scrollTop() / 225);
 });
 
 $(window).scroll(function(){
-	$(".headerbackground").css("opacity", $(window).scrollTop() / 600);
+	$(".backgroundhistory").css("opacity", 1 - $(window).scrollTop() / 225);
+});
+
+$(window).scroll(function(){
+	$(".backgroundimpact").css("opacity", 1 - $(window).scrollTop() / 225);
+});
+
+$(window).scroll(function(){
+	$(".headerbackground").css("opacity", $(window).scrollTop() / 225);
 });
